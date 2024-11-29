7cebc12b4117a4f5205b5c0831608df54d6d0b6b
beisenmiyi <3502771302@qq.com>
2024-11-30 00:12:47 +0800

    修复聊天记录无法上下滑动的问题

--- frontend/windows/updateResultWindow/updateResultWindow.js
+++ frontend/windows/updateResultWindow/updateResultWindow.js
@@ -8,7 +8,7 @@ window.myAPI.onUpdateAvailable((event, info) => {
 
 //监听更新错误信息
 window.myAPI.onError((event, error) => {
-    updateResultWindow.textContent = error;
+    updateResultWindow.textContent = "检查更新出错啦，可能是您的网络问题，请稍后再试，或者前往https://github.com/beisenmiyi/BsmyChat/releases下载最新版本";
 });
 
 //监听下载进度信息
